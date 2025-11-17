a982b7bdcd061a3104c710df1e622af3fd7b5820
Leandro Mota Leal <leandro.leal@al.infnet.edu.br>
2025-11-17 10:00:00 -0300

    17/11/2025 - ValidaÃ§Ãµes avanÃ§adas (datas, enums) (2h)
    CriaÃ§Ã£o de validaÃ§Ãµes mais complexas: datas, enums,
    combinaÃ§Ãµes de campos e testes de cenÃ¡rios invÃ¡lidos.
    
    Categoria: ValidaÃ§Ãµes e Qualidade
    Horas: 2

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import * as ctrl from "../controllers/categorias.controller.js";
+import { 
+    validateId, 
+    validateCategoriaCriar, 
+    validateCategoriaAtualizar 
+} from '../middlewares/validators.js';
 
 const r = Router();
 r.get("/", ctrl.list);
-r.get("/:id", ctrl.get);
-r.post("/", ctrl.create);
-r.put("/:id", ctrl.update);
-r.delete("/:id", ctrl.remove);
+r.get("/:id", validateId, ctrl.get);
+r.post("/", validateCategoriaCriar, ctrl.create);
+r.put("/:id", validateId, validateCategoriaAtualizar, ctrl.update);
+r.delete("/:id", validateId, ctrl.remove);
 
-export default r; // << importante
+export default r; 
--- src/routes/organizacoes.routes.js
+++ src/routes/organizacoes.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import organizacoesController from '../controllers/organizacoes.controller.js';
+import { 
+    validateId, 
+    validateOrganizacaoCriar, 
+    validateOrganizacaoAtualizar 
+} from '../middlewares/validators.js';
 
 const router = Router();
 
 router.get('/', organizacoesController.listar);
-router.get('/:id', organizacoesController.buscarPorId);
-router.post('/', organizacoesController.criar);
-router.put('/:id', organizacoesController.atualizar);
-router.delete('/:id', organizacoesController.excluir);
+router.get('/:id', validateId, organizacoesController.buscarPorId);
+router.post('/', validateOrganizacaoCriar, organizacoesController.criar);
+router.put('/:id', validateId, validateOrganizacaoAtualizar, organizacoesController.atualizar);
+router.delete('/:id', validateId, organizacoesController.excluir);
 
 export default router;
